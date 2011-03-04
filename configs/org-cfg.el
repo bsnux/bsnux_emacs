b919a7dbaff96641459e914255a147b0d8e0c524
Arturo Fernandez <arturo@bsnux.com>
2011-03-04 11:23:40 +0100

    Example of using GTD with org-mode

--- configs/org-cfg.el
+++ configs/org-cfg.el
@@ -1,5 +1,12 @@
 ;; MobileOrg + Dropbox + org-mode
 ;; Don't forget to create your ~/Dropbox/MobileOrg folder!
+;;
+;; Example of GTD task: 
+;; ** TODO [#A] Buy milk :personal:
+;;    DEADLINE: <2011-03-11 Fri>
+;; Where:
+;;    [#A] => Priority from 'A' to 'D'
+;;    :personal: => Tag 
 
 ;; Set to the location of your Org files on your local system
 (setq org-directory "~/org")
