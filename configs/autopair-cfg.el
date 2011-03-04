6fb1b23cb40ff2024fe2b6254f4517ef9882060a
Arturo Fernandez <arturo@bsnux.com>
2011-03-04 10:02:44 +0100

    Loading autopair-mode only for code files as Python, C++, etc

--- configs/autopair-cfg.el
+++ configs/autopair-cfg.el
@@ -1,2 +1,22 @@
 (require 'autopair)
-(autopair-global-mode)
+
+(add-hook 'c-mode-common-hook 
+           #'(lambda () (autopair-mode)))
+
+(add-hook 'c++-mode-common-hook 
+           #'(lambda () (autopair-mode)))
+
+(add-hook 'python-mode-hook 
+           #'(lambda () (autopair-mode)))
+
+(add-hook 'css-mode-hook 
+           #'(lambda () (autopair-mode)))
+
+(add-hook 'js2-mode-hook 
+           #'(lambda () (autopair-mode)))
+
+(add-hook 'php-mode-hook 
+           #'(lambda () (autopair-mode)))
+
+(add-hook 'ruby-mode-hook 
+           #'(lambda () (autopair-mode)))
--- configs/python-cfg.el
+++ configs/python-cfg.el
@@ -3,3 +3,9 @@
 (add-hook 'python-mode-hook '(lambda () 
      (define-key python-mode-map "\C-m" 'newline-and-indent)))
 (setq-default py-indent-offset 4)
+(add-hook 'python-mode-hook
+           #'(lambda ()
+               (setq autopair-handle-action-fns
+                     (list #'autopair-default-handle-action
+                           #'autopair-python-triple-quote-action))))
+
