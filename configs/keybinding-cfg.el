1beaacdc1d8d301e64ea6571ca3a4041b92a7d05
Arturo Fernandez <arturo@bsnux.com>
2011-03-09 17:42:46 +0100

    Adding new keybindg for lusty-explorer. Redefining C-x C-f and C-x b

--- configs/keybinding-cfg.el
+++ configs/keybinding-cfg.el
@@ -4,6 +4,8 @@
 (global-set-key [f3] 'shell)
 (global-set-key [f4] 'indent-region)
 ; lusty
+(global-set-key "\C-x\C-f" 'lusty-file-explorer)
+(global-set-key (kbd "C-x b") 'lusty-buffer-explorer)
 (global-set-key [f5] 'lusty-file-explorer)
 (global-set-key [f6] 'lusty-buffer-explorer)
 ;		Copy and paste current line
