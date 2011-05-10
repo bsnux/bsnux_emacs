39ac577b63722f4edefb792ab6db854e5c72f36b
bsnux <arturo@bsnux.com>
2011-05-10 09:49:39 +0200

    Adding zen-and-art theme

--- configs/color-theme-cfg.el
+++ configs/color-theme-cfg.el
@@ -4,5 +4,6 @@
 (load-file "~/.emacs.d/vendor/color-theme/themes/color-theme-github.el")
 (load-file "~/.emacs.d/vendor/color-theme/themes/color-theme-wombat.el")
 (load-file "~/.emacs.d/vendor/color-theme/themes/color-theme-merbivore.el")
+(load-file "~/.emacs.d/vendor/color-theme/themes/zen-and-art.el")
 
 (color-theme-wombat)
