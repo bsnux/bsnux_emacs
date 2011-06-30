cd5ce54c5f0e10504f0e90a8afe3bb842fda5941
Arturo Fernandez <arturo@bsnux.com>
2011-06-30 19:32:20 +0200

    Setting a new file on Dropbox for 'org-agenda'

--- configs/org-cfg.el
+++ configs/org-cfg.el
@@ -21,8 +21,7 @@
 ;; Set to the location of your Org files on your local system
 (setq org-directory "~/org")
 ;; Files for syncing
-(setq org-agenda-files (quote ( "~/org/gtd.org"
-                                "~/org/notes.org")))
+(setq org-agenda-files (quote ( "~/Dropbox/gtd.org")))
 ;; Set to the name of the file where new notes will be stored
 (setq org-mobile-inbox-for-pull "~/org/flagged.org")
 ;; Set to <your Dropbox root directory>/MobileOrg.
