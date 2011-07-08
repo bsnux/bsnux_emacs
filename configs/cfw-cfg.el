e31887bc8e70b03e91c8535256709ef18a16b64d
Arturo Fernandez <arturo@bsnux.com>
2011-07-08 11:15:24 +0200

    Deleting tests for using different themes per mode

--- configs/cfw-cfg.el
+++ configs/cfw-cfg.el
@@ -2,9 +2,3 @@
 
 ; cfw:open-org-calendar => Displays calendar using org-agenda files
 (require 'calfw-org)
-
-(defun cfw:calendar-mode-theme-hook ()
-  (require 'color-theme)
-  (color-theme-initialize)
-  (color-theme-github))
-(add-hook 'cfw:calendar-mode 'cfw:calendar-mode-theme-hook)
