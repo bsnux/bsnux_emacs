325949695bdcd746e939b0547409f34cac81aac1
Arturo Fernandez <arturo@bsnux.com>
2011-10-10 17:11:51 +0200

    Adding two new color-themes

--- configs/color-theme-cfg.el
+++ configs/color-theme-cfg.el
@@ -7,7 +7,12 @@
 (load-file "~/.emacs.d/vendor/color-theme/themes/zen-and-art.el")
 (load-file "~/.emacs.d/vendor/color-theme/themes/color-theme-molokai.el")
 (load-file "~/.emacs.d/vendor/color-theme/themes/color-theme-leuven.el")
+(load-file "~/.emacs.d/vendor/color-theme/themes/sweyla516816.el")
+(load-file "~/.emacs.d/vendor/color-theme/themes/sweyla834976.el")
 
 (require 'color-theme-solarized)
+(require 'sweyla516816)
+(require 'sweyla834976)
+
 
 (color-theme-wombat)
