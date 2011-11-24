240c40eefda71c861e11deeb090b29fe1ce5b7d2
Arturo Fernandez <arturo@bsnux.com>
2011-11-24 11:28:41 +0100

    Using flake8 instead of pyflakes for checking Python code

--- configs/pyflake-cfg.el
+++ configs/pyflake-cfg.el
@@ -1,5 +1,9 @@
+;; Cheking Python code
+;;----------------------
+;; Installing a checker, for example:
+;;     $ pip install flake8
 ;; While visiting a Python file, run:
-;; M-x python-check or
-;; C-c C-v or
-;; C-c C-w
-(setq python-check-command "pyflakes")
+;;     M-x python-check or
+;;     C-c C-v or
+;;     C-c C-w
+(setq python-check-command "flake8")
