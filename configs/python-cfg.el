b7dded0ddbabea590a4d15b5414bb7420263ef43
Arturo Fernandez <arturo@bsnux.com>
2011-11-24 11:54:16 +0100

    Invoking flymake by default for Python files

--- configs/python-cfg.el
+++ configs/python-cfg.el
@@ -5,6 +5,7 @@
 (setq-default py-indent-offset 4)
 (add-hook 'python-mode-hook
            #'(lambda ()
+               (unless (eq buffer-file-name nil) (flymake-mode 1))
                (setq autopair-handle-action-fns
                      (list #'autopair-default-handle-action
                            #'autopair-python-triple-quote-action))))
